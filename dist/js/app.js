4eeafe06c4c7de2e7d4ecc6851649f7efb97aaf7
okotovenko <oakotovenko@ukr.net>
2024-04-14 13:52:27 +0300

    6

--- dist/js/app.js
+++ dist/js/app.js
@@ -599,6 +599,52 @@
         document.addEventListener("click", (function(e) {
             let targetElement = e.target;
             if (targetElement.closest("[data-text]") && targetElement.closest(".services__item")) document.querySelector(".input-textarea").value = targetElement.innerText;
+            if (targetElement.closest("[data-img]") && targetElement.closest(".certificates__img")) document.querySelector(".popup__link").src = targetElement.src;
+        }));
+    }
+    function addPaginationToProjectPage() {
+        document.addEventListener("DOMContentLoaded", (function() {
+            const content = document.querySelector(".projects__container.projects__container--project");
+            if (content) {
+                const itemsPerPage = 4;
+                let currentPage = 0;
+                const items = Array.from(content.getElementsByTagName("article")).slice(0);
+                console.log(items);
+                function showPage(page) {
+                    const startIndex = page * itemsPerPage;
+                    const endIndex = startIndex + itemsPerPage;
+                    items.forEach(((item, index) => {
+                        item.classList.toggle("hidden", index < startIndex || index >= endIndex);
+                    }));
+                    updateActiveButtonStates();
+                }
+                function createPageButtons() {
+                    const totalPages = Math.ceil(items.length / itemsPerPage);
+                    const paginationContainer = document.createElement("nav");
+                    const paginationDiv = document.body.appendChild(paginationContainer);
+                    paginationContainer.classList.add("navigation", "pagination");
+                    for (let i = 0; i < totalPages; i++) {
+                        const pageButton = document.createElement("button");
+                        pageButton.classList.add("page-numbers");
+                        pageButton.textContent = i + 1;
+                        pageButton.addEventListener("click", (() => {
+                            currentPage = i;
+                            showPage(currentPage);
+                            updateActiveButtonStates();
+                        }));
+                        content.appendChild(paginationContainer);
+                        paginationDiv.appendChild(pageButton);
+                    }
+                }
+                function updateActiveButtonStates() {
+                    const pageButtons = document.querySelectorAll(".pagination button");
+                    pageButtons.forEach(((button, index) => {
+                        if (index === currentPage) button.classList.add("active"); else button.classList.remove("active");
+                    }));
+                }
+                createPageButtons();
+                showPage(currentPage);
+            }
         }));
     }
     window["FLS"] = true;
@@ -613,4 +659,5 @@
     preloader();
     activeMenu();
     openForm();
+    addPaginationToProjectPage();
 })();
